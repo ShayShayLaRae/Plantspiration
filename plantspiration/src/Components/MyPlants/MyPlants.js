9c26305b170d1d998529792f325e3f94f91801b3
Shayla Paulsen <shayla.paulsen@gmail.com>
2019-11-20 14:37:43 -0700

    first commit

---
+++ plantspiration/src/Components/MyPlants/MyPlants.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import './MyPlants.css';
+
+
+export default class MyPlants extends Component{
+    render() {
+        return(
+            <div>
+                MyPlants
+            </div>
+        )
+    }
+}
---
+++ plantspiration/src/Components/Wishlist/Wishlist.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import './Wishlist.css';
+
+
+export default class Wishlist extends Component{
+    render() {
+        return(
+            <div>
+                Wishlist
+            </div>
+        )
+    }
+}
